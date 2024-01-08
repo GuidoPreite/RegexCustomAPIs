016cdf1c6493a25bbb249130f7fed5f2870c76d5
Guido Preite <43754988+GuidoPreite@users.noreply.github.com>
2024-01-08 20:55:44 +0100

    Add files via upload

---
+++ RegexCustomAPIs.Plugins/RegexMatch.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Text.RegularExpressions;
+
+namespace RegexCustomAPIs.Plugins
+{
+    public class RegexMatch : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            if (context.MessageName.Equals("rca_RegexMatch") && context.Stage.Equals(30))
+            {
+                string input = context.InputParameters["Input"] as string;
+                string pattern = context.InputParameters["Pattern"] as string;
+
+                bool success = false;
+                int index = 0;
+                string value = "";
+                bool error = false;
+                string errorMessage = "";
+                try
+                {
+                    Match match = Regex.Match(input, pattern);
+                    success = match.Success;
+                    index = match.Index;
+                    value = match.Value;
+                }
+                catch (Exception ex)
+                {
+                    error = true;
+                    errorMessage = ex.Message;
+                }
+
+                context.OutputParameters["Success"] = success;
+                context.OutputParameters["Index"] = index;
+                context.OutputParameters["Value"] = value;
+                context.OutputParameters["Error"] = error;
+                context.OutputParameters["ErrorMessage"] = errorMessage;
+            }
+        }
+    }
+}
---
+++ RegexCustomAPIs.Plugins/RegexReplace.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Text.RegularExpressions;
+
+namespace RegexCustomAPIs.Plugins
+{
+    public class RegexReplace : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            if (context.MessageName.Equals("rca_RegexReplace") && context.Stage.Equals(30))
+            {
+                string input = context.InputParameters["Input"] as string;
+                string pattern = context.InputParameters["Pattern"] as string;
+                string replacement = context.InputParameters["Replacement"] as string;
+
+                string result = "";
+                bool error = false;
+                string errorMessage = "";
+                try
+                {
+                    result = Regex.Replace(input, pattern, replacement);
+                }
+                catch (Exception ex)
+                {
+                    error = true;
+                    errorMessage = ex.Message;
+                }
+
+                context.OutputParameters["Result"] = result;
+                context.OutputParameters["Error"] = error;
+                context.OutputParameters["ErrorMessage"] = errorMessage;
+            }
+        }
+    }
+}
